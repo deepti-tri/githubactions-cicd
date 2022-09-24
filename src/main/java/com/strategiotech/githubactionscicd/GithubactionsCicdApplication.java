b351a3f0d964b957eca046ef7926eb0c7d64f5ff
Riya Yadav <59980457+Riya06june@users.noreply.github.com>
2022-09-24 13:32:21 -0400

    Update GithubactionsCicdApplication.java

--- src/main/java/com/strategiotech/githubactionscicd/GithubactionsCicdApplication.java
+++ src/main/java/com/strategiotech/githubactionscicd/GithubactionsCicdApplication.java
@@ -14,7 +14,7 @@ public class GithubactionsCicdApplication {
         SpringApplication.run(GithubactionsCicdApplication.class, args);
     }
 
-    @GetMapping("/hello")
+    @RequestMapping("/hello")
     public String hello(@RequestParam(value = "name", defaultValue = "Hello World") String name) {
         return name;
 
