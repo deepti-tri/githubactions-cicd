a65caa20a196bdd4df5f4ef028d58ae8794c46f5
Riya Yadav <59980457+Riya06june@users.noreply.github.com>
2022-09-24 13:44:44 -0400

    Update GithubactionsCicdApplication.java

--- src/main/java/com/strategiotech/githubactionscicd/GithubactionsCicdApplication.java
+++ src/main/java/com/strategiotech/githubactionscicd/GithubactionsCicdApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 @RestController
@@ -14,7 +15,7 @@ public class GithubactionsCicdApplication {
         SpringApplication.run(GithubactionsCicdApplication.class, args);
     }
 
-    @GetMapping("/hello")
+    @RequestMapping("/")
     public String hello(@RequestParam(value = "name", defaultValue = "Hello World") String name) {
         return name;
 
